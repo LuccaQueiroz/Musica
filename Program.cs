c542aa66354e3ecd23983cfc14c9ab5b068207ea
Lucas <lucasqueirozzwerner@gmail.com>
2022-09-21 19:54:48 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("O que pensa que eu sou - Banda Djavú\n");
+            Console.ResetColor();
+            Console.WriteLine("Me lembro quando me deixou sozinha");
+            Console.WriteLine("Amor, o que eu fiz pra merecer?");
+            Console.WriteLine("Se pensa que eu sou sua menina");
+            Console.WriteLine("Não sou um brinquedo pra você\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Eu quero te possuir");
+            Console.WriteLine("Quero você todinha pra mim, vai");
+            Console.WriteLine("Eu quero te possuir, como é que é?");
+            Console.WriteLine("Quero você todinha pra mim\n");
+            Console.ResetColor();
+
+            Console.WriteLine("O que pensa que eu sou");
+            Console.WriteLine("(Se não sou o que pensou)");
+            Console.WriteLine("E me libera, não insista");
+            Console.WriteLine("Vai viver um outro amor\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("O que (pensa que eu sou)");
+            Console.WriteLine("Se (não sou o que pensou)");
+            Console.WriteLine("E me libera, não insista");
+            Console.ResetColor();
+
